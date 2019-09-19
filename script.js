1197c77c9c817b8edf501811f7a0b8ce276b9f79
Siddanth Varanasi <siddhanth.varanasi2003@gmail.com>
2019-09-19 12:54:34 +0200

    added form button

--- script.js
+++ script.js
@@ -24,17 +24,18 @@ function nextPage()
 
 function option()
 {
-    var btn1 = document.createElement("BUTTON");
-    btn1.innerHTML = "Form";
-    document.body.appendChild(btn1);
-    form(btn1);
+    var btn1 = document.createElement("BUTTON");    
+    btn1.innerText = "New button created"
+    btn1.onclick = function () {
+        form();
+    }
     
+    document.body.appendChild(btn1);
 }
 
-function form(x)
-{   
-    if (x == clicked)
-     {
-        alert("yes");
-     }
+function form()
+{ 
+    var bootstrapForm = document.getElementById("bootstrap-form");
+    bootstrapForm.style =  "display:inital";
+    alert("yes");
 }
